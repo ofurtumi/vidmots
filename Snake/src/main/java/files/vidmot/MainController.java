725c6c42ef6c8f81216ad3c69f291aa87fe2335a
Tumi <thorvaldur.tumi@gmail.com>
2022-03-04 11:48:01 +0000

    bara til öryggis ef allt fer i fokk

--- Snake/src/main/java/files/vidmot/MainController.java
+++ Snake/src/main/java/files/vidmot/MainController.java
@@ -5,12 +5,14 @@ import javafx.animation.Timeline;
 import javafx.animation.Animation.Status;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Bounds;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
 import javafx.util.Duration;
 
@@ -33,10 +35,17 @@ public class MainController implements Initializable {
 
     private ArrayList<Rectangle> snakePieces = new ArrayList<Rectangle>();
 
+    private Circle foodItem;
+    private Bounds foodBounds;
+
     private KeyFrame k;
     private Timeline t;
 
+    private ArrayList<snake> enemies = new ArrayList<snake>();
+
     // private Scene scene = pane.getScene();
+
+    private playerSnake ps;
 
     @FXML
     private void handleButtonAction(MouseEvent event) {
@@ -48,14 +57,32 @@ public class MainController implements Initializable {
     private HashMap<Integer, Tuple> moveMap = new HashMap<Integer, Tuple>();
 
     private void createTimeline() {
-        Rectangle r = snakePieces.get(0);
         k = new KeyFrame(Duration.millis(50), e -> {
-            checkSetColor(r);
-            moveRect(r);
-            for (int i = 1; i < snakePieces.size(); i++) {
-                tail(snakePieces.get(i), snakePieces.get(i - 1));
+            // checkSetColor(r);
+            addFood();
+
+            // enemies[0].move();
+            for (snake enemy : enemies) {
+                if (enemy == null)
+                    continue;
+                enemy.moveRandom();
+                if (ps.intersects(enemy.getBoundsInParent())) {
+                    // System.out.println("Dead :/ at " + r.getX());
+                    timeLineController(2);
+                }
             }
-            // tail(tail, r);
+
+            if (ps.intersects(foodBounds)) {
+                pane.getChildren().remove(foodItem);
+                // System.out.println("nammi namm :)~");
+                addTailPiece();
+                pane.getChildren().add(ps.addTailPiece());
+                score++;
+                scoreLabel.setText(Integer.toString(score));
+            }
+
+            ps.move();
+            ps.tailMover();
         });
         t = new Timeline(k);
         t.setCycleCount(999999999);
@@ -74,29 +101,30 @@ public class MainController implements Initializable {
         }
     }
 
-    private void checkSetColor(Rectangle r) {
-        if (r.getFill().equals(Color.BLUE))
-            r.setFill(Color.RED);
-        else
-            r.setFill(Color.BLUE);
-    }
+    // uncommenta þetta fyrir löggusnák
+    // private void checkSetColor(Rectangle r) {
+    // if (r.getFill().equals(Color.BLUE))
+    // r.setFill(Color.RED);
+    // else
+    // r.setFill(Color.BLUE);
+    // }
 
     private void moveRect(Rectangle r) {
         int rotation = (int) r.getRotate();
         Tuple movement = (Tuple) moveMap.get(rotation);
 
-        if (rotation == 0 && r.getX() >= 475)
+        if (rotation == 0 && r.getX() >= pane.getWidth() - 25)
             r.setX(-25);
 
         else if (rotation == 180 && r.getX() <= -25)
-            r.setX(475);
+            r.setX(pane.getWidth() - 25);
         else
             r.setX(r.getX() + movement.x);
 
-        if (rotation == 90 && r.getY() >= 475)
+        if (rotation == 90 && r.getY() >= pane.getHeight() - 25)
             r.setY(-25);
         else if (rotation == 270 && r.getY() <= -25)
-            r.setY(475);
+            r.setY(pane.getHeight() - 25);
         else
             r.setY(r.getY() + movement.y);
 
@@ -129,12 +157,14 @@ public class MainController implements Initializable {
     }
 
     private void start(Scene scene) {
-        Rectangle r = new Rectangle(50, 50, Color.RED);
+        addEnemy();
+        Rectangle r = new Rectangle(50, 50, Color.GREEN);
         r.setX(50);
         r.setY(50);
         snakePieces.add(r);
+        ps = new playerSnake(pane);
 
-        pane.getChildren().addAll(r, scoreLabel);
+        pane.getChildren().addAll(r, scoreLabel, ps);
 
         r.setOnMouseDragged(event -> {
             r.setX(event.getX() - 50);
@@ -151,15 +181,19 @@ public class MainController implements Initializable {
                 switch (k) {
                     case RIGHT:
                         r.setRotate(0);
+                        ps.rotate(0);
                         break;
                     case DOWN:
                         r.setRotate(90);
+                        ps.rotate(90);
                         break;
                     case LEFT:
                         r.setRotate(180);
+                        ps.rotate(180);
                         break;
                     case UP:
                         r.setRotate(270);
+                        ps.rotate(270);
                         break;
                     default:
                         break;
@@ -167,7 +201,6 @@ public class MainController implements Initializable {
             }
 
             if (e.getCode() == KeyCode.SPACE) {
-                addTailPiece();
             }
 
             if (e.getCode() == KeyCode.ESCAPE) {
@@ -182,13 +215,33 @@ public class MainController implements Initializable {
     }
 
     private void addTailPiece() {
+        // todo stóra, bæta við röndum
         snakePieces.add(new Rectangle(50, 50));
         Rectangle piece = snakePieces.get(snakePieces.size() - 1);
         piece.setX(snakePieces.get(snakePieces.size() - 2).getX());
         piece.setY(snakePieces.get(snakePieces.size() - 2).getY());
         pane.getChildren().add(piece);
-        score++;
-        scoreLabel.setText(Integer.toString(score));
+    }
+
+    private void addFood() {
+        if (!pane.getChildren().contains(foodItem)) {
+            foodItem = new Circle(25, Color.RED);
+            int yCoord = ((int) (Math.random() * pane.getHeight()) + 25);
+            yCoord = yCoord - (yCoord % 50);
+            int xCoord = ((int) (Math.random() * pane.getWidth()) + 25);
+            xCoord = xCoord - (xCoord % 50);
+
+            foodItem.setCenterY(yCoord);
+            foodItem.setCenterX(xCoord);
+            foodBounds = foodItem.getBoundsInParent();
+            pane.getChildren().add(foodItem);
+        }
+    }
+
+    private void addEnemy() {
+        snake s = new snake(pane);
+        enemies.add(s);
+        pane.getChildren().add(s);
     }
 
     @Override
---
+++ Snake/src/main/java/files/vidmot/snake.java
@@ -0,0 +1,90 @@
+package files.vidmot;
+
+import java.util.HashMap;
+
+import files.vinnsla.Tuple;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+
+public class snake extends Rectangle {
+    private HashMap<Integer, Tuple> moveMap = new HashMap<Integer, Tuple>();
+    private int counter = 0;
+    private Pane pane;
+    // þarf að halda utan um pane til að geta farið í gegn um vegg
+    // eftir að skjárinn er stækkaður
+
+    public snake(Pane p) {
+        this.setWidth(150);
+        this.setHeight(50);
+        this.setFill(Color.ORANGE);
+        this.setX(250);
+        this.setY(250);
+        this.pane = p;
+
+        onStart();
+    }
+
+    public void moveRandom() {
+        if (counter++ == 50) {
+            rotateRandom();
+            counter = 0;
+        }
+        if (counter % 2 == 0) {
+            move();
+        }
+    }
+
+    public void move() {
+        int rotation = (int) this.getRotate();
+        Tuple movement = (Tuple) moveMap.get(rotation);
+
+        if (rotation == 0 && this.getX() >= pane.getWidth() - (this.getWidth()/2))
+            this.setX(-(this.getWidth()/2));
+
+        else if (rotation == 180 && this.getX() <= -(this.getWidth()/2))
+            this.setX(pane.getWidth() - (this.getWidth()/2));
+        else
+            this.setX(this.getX() + movement.x);
+
+        if (rotation == 90 && this.getY() >= pane.getHeight() - (this.getWidth()/2))
+            this.setY(-(this.getWidth()/2));
+        else if (rotation == 270 && this.getY() <= -(this.getWidth()/2))
+            this.setY(pane.getHeight() - (this.getWidth()/2));
+        else
+            this.setY(this.getY() + movement.y);
+    }
+
+    private void rotateRandom() {
+        int direction = (int) (Math.random() * 4);
+        switch (direction) {
+            case 0:
+                this.setRotate(0);
+                break;
+            case 1:
+                this.setRotate(90);
+                break;
+            case 2:
+                this.setRotate(180);
+                break;
+            case 3:
+                this.setRotate(270);
+                break;
+            default:
+                break;
+
+        }
+    }
+
+    private void onStart() {
+        Tuple RIGHT = new Tuple(25, 0);
+        Tuple LEFT = new Tuple(-25, 0);
+        Tuple UP = new Tuple(0, -25);
+        Tuple DOWN = new Tuple(0, 25);
+
+        moveMap.put(0, RIGHT);
+        moveMap.put(90, DOWN);
+        moveMap.put(180, LEFT);
+        moveMap.put(270, UP);
+    }
+}
